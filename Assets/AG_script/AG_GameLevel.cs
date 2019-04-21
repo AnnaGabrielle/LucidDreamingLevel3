2a0c797f59d9696019b8e5e85ec48d7fef5e1d61
= <=>
2019-04-21 19:14:18 -0400

    sound+particles+enemyCount

--- Assets/AG_script/AG_GameLevel.cs
+++ Assets/AG_script/AG_GameLevel.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class AG_GameLevel : MonoBehaviour {
 
 	[SerializeField] int ag_Enemies;
 	[SerializeField] float ag_loadWinDelay =1f;
     [SerializeField] string ag_Level4;
+	[SerializeField] Text ag_enemy_number;
 	//Tags name
 	public string ag_EnemiesTAG;
 
+	void Update(){
+		ag_enemy_number.text = ag_Enemies.ToString();
+	}
 	public void AG_CountEnemies(){
 		ag_Enemies++;
 	}
